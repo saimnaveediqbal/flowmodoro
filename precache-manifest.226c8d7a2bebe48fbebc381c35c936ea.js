0dce907847510ea6dc822ba9864dbb8b3375d951
Saim Iqbal <saim.iqbal@shifthyperloop.com>
2023-01-17 13:49:55 +0100

    Updates

---
+++ precache-manifest.226c8d7a2bebe48fbebc381c35c936ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7af83449fe41f38d9c667978c3d9dec",
+    "url": "/flowmodoro/index.html"
+  },
+  {
+    "revision": "c1389bc34ac0918bb090",
+    "url": "/flowmodoro/static/css/main.54578419.chunk.css"
+  },
+  {
+    "revision": "d9440a083fd86d2ced75",
+    "url": "/flowmodoro/static/js/2.3527a53a.chunk.js"
+  },
+  {
+    "revision": "b4c639f0ee23385dbf7c19ddd9d0c87d",
+    "url": "/flowmodoro/static/js/2.3527a53a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43468745a2b08be00a36",
+    "url": "/flowmodoro/static/js/3.d9f55562.chunk.js"
+  },
+  {
+    "revision": "c1389bc34ac0918bb090",
+    "url": "/flowmodoro/static/js/main.d9d7d22f.chunk.js"
+  },
+  {
+    "revision": "3ad94c85ec44db6be025",
+    "url": "/flowmodoro/static/js/runtime-main.de124b38.js"
+  }
+]);
